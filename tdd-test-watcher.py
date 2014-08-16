f9faac24114591a57c7633a322d0b5eeb407dbc9
Antoine Albertelli <antoinea101@gmail.com>
2014-08-16 18:09:58 +0200

    Add a utility to automatically run tests
    This little python program can run the tests after a change in a file
    described in the package.yml file.

---
+++ tdd-test-watcher.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+"""
+This little utility can read a package.yml and run the unit tests when one of
+the source or test file changes.
+
+It is very useful when using a TDD-style of programming because you will often
+find yourself doing the same steps: building and running a test after each file
+change. It is also editor-independent, which is great :)
+"""
+
+import packager
+import yaml
+import os.path
+from time import sleep
+import subprocess
+
+try:
+    from termcolor import cprint
+except:
+    def cprint(msg, color):
+        print(msg)
+
+
+def run_tests(changed_path):
+    """
+    Run all the tests after a change in changed_path.
+    """
+    failure = subprocess.call("make -C build/".split())
+
+    if failure:
+        cprint('Build failed after {} changed!'.format(changed_path), 'red')
+        return
+
+    failure = subprocess.call("build/tests".split())
+
+    if failure:
+        cprint('Tests failed after {} changed!'.format(changed_path), 'red')
+        return
+
+    cprint('All OK', 'green')
+
+def main():
+    package = yaml.load(open("package.yml").read())
+    sources = packager.generate_source_dict(package)
+
+    if len(sources['tests']) == 0:
+        print('No unit tests ? Aborting !')
+        return
+
+    files = sources['tests'] + sources['source']
+    modtimes = {}
+
+    for path in files:
+        modtimes[path] = os.path.getmtime(path)
+
+    while True:
+        for path in files:
+            # Vim deletes file before writing so we have to check for
+            # FileNotFoundError
+            try:
+                mtime = os.path.getmtime(path)
+            except FileNotFoundError:
+                continue
+
+            # If the file changed, run the build / tests
+            if modtimes[path] != mtime:
+                run_tests(path)
+                modtimes[path] = mtime
+
+        # Avoid full CPU usage
+        sleep(1)
+
+if __name__ == "__main__":
+    main()
